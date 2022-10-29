64cc0e4aeb967025abf8eb333f5543211fa38479
audreythando <audreythando@gmail.com>
2022-10-29 09:17:58 +0200

    refactor: added styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import { Box, Button, Img } from "@chakra-ui/react";
+import Link from "next/link";
+const Navbar = () => {
 
-function Navbar() {
-  return (
-    <nav>
-        <div className='logo'>
-        <h1>Navbar</h1>
-        </div>
-        <a>Home</a>
-        <a>Login</a>
-        <a>SignUp</a>
-    </nav>
-  )
+    return(
+            <>
+            <Box>
+            <Img  src='.\Images\logo.png' alt='logo' width={'223px'} height={'86px'} position={'absolute'} alignItems={'flex-start'}/>
+            </Box>
+            <Box width={'252px'} height={'46px'} position={'absolute'} left={'500px'} marginTop={'20px'}>
+            <Link href='/signUp'> <a>SignUp</a> </Link>
+            </Box>
+               <Box width={'252px'} height={'46px'} position={'absolute'} left={'700px'} marginTop={'20px'}> 
+               <Link href='/login'> <a>Login</a> </Link>
+             </Box> 
+            <Box >
+             <Button width={'240px'} height={'50px'} colorScheme={'orange'} border-radius={'15px'} position={'absolute'} right={'70px'}>Add Lesson Plan</Button> 
+            </Box>
+             </>
+    )
 }
 
-export default Navbar
+export default Navbar;
