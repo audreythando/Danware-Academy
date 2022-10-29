59256583eac063a71098ed84567c5276ed904756
audreythando <audreythando@gmail.com>
2022-10-29 09:18:28 +0200

    refactor:added and removed code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,15 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+ 
+      
       <Footer/>
     </div>
   )
