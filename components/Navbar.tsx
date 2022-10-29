925fb9c2f205a50cab5e8e878a43f5a783cf444e
audreythando <audreythando@gmail.com>
2022-10-29 06:52:24 +0200

    refactor: created Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <div className='logo'>
+        <h1>Navbar</h1>
+        </div>
+        <a>Home</a>
+        <a>Login</a>
+        <a>SignUp</a>
+    </nav>
+  )
+}
+
+export default Navbar
