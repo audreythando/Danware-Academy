f4af99a49786d886a532bddb8029022760136be4
audreythando <audreythando@gmail.com>
2022-10-29 09:17:23 +0200

    refactor: created 404 and added code and styling

---
+++ pages/404.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link';
+import { Image, Heading,Box, Flex, Button, useMediaQuery, Center } from '@chakra-ui/react';
+
+
+function NotFound() {
+  const [isResponsive] = useMediaQuery('(max-width: 400px)')
+
+
+  return (
+
+    <div  >
+       <Box textAlign={'center'} height={'100%'} width={'100%'}  >
+       <Center alignItems={'center'} >
+       <Image src='./Images/logo.png' alt="logo" width={isResponsive ? '228px':'465px'} height={isResponsive ? '54px' :'108px'} />
+       </Center>
+       <Box  >
+       <Flex alignItems={'center'} justifyContent={'center'} direction={'column'} >
+        
+        <Box textAlign={'center'} width={'434px'} height={'68px'} marginTop={'50px'}  >
+       <Heading fontStyle={'normal'} fontFamily={'Roboto'} fontWeight={isResponsive? '350px':'700px'} fontSize={isResponsive?'15px':'30px'} lineHeight={'30px'}>
+        Page Not Found</Heading>
+       
+        <Image src='.\Images\404.png' alt='' marginTop={'20px'} width={isResponsive? '260px':'521px'} height={isResponsive?'138px':'276px'}   /> 
+           <Link href="/">
+          <Button colorScheme='orange' width={isResponsive?'153px':'307px'} height={isResponsive?'28px':'56px'} marginTop={'30px'}>Back to home page</Button> 
+          </Link>
+         </Box>
+        
+       
+        </Flex>
+       </Box>
+       </Box>
+    </div>
+  )
+}
+
+export default NotFound
