bb9d1b927811ea36bc4a4ff4e7443b7d885322fa
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2024-05-16 19:54:11 +0200

    fix build errors

---
+++ dist/next.config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
+module.exports = nextConfig;
+//# sourceMappingURL=next.config.dev.js.map
